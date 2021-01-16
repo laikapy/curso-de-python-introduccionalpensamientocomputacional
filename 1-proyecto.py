168629df3444ef207aef86370c0a6925d723c922
Luis Angel Juarez M <petela.contacto@gmail.com>
2021-01-15 22:22:20 -0600

    Esta es ka tercer versuib eb donde incluimos un break

--- 1-proyecto.py
+++ 1-proyecto.py
@@ -1,6 +1,9 @@
 # proyecto de ramificaciones if
 contador = 0
 
-while contador < 10:
+while contador < 100:
     print(contador)
-    contador +=1 
+    contador +=1
+    if contador == 50:
+        print('Ya llegamos a la mitad del contador y aplicaremos un break')
+        break

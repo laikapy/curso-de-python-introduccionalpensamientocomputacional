92fbbe2f799c7eafbf325c4539734378a2f6858c
Luis Angel Juarez M <petela.contacto@gmail.com>
2021-01-16 21:13:31 -0600

    segunda version del programa

--- 1-proyecto.py
+++ 1-proyecto.py
@@ -1,3 +1,4 @@
+"""
 # proyecto de ramificaciones if
 contador = 0
 
@@ -7,3 +8,13 @@ while contador < 100:
     if contador == 50:
         print('Ya llegamos a la mitad del contador y aplicaremos un break')
         break
+"""
+x = 0.0
+for i in range(10):
+    x += 0.1
+    print(x)
+
+if x == 1.0:
+    print(f'x = {x}')
+else:
+    print(f'x != {x}')
